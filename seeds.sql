f19d58eef90238eaeb78b313af8bb2d7602bde99
Stitch <john@comteque.com.au>
2020-08-18 20:12:36 +0800

    new index

---
+++ seeds.sql
@@ -0,0 +1,4 @@
+insert into events (title, description, category, suburb, dateTime, createdAt, updatedAt)
+values ('Coogee Beach Cleanup', 'Coogee Beach Cleanup description goes here...', 'Cleanup', 'Coogee, WA', ('2020-08-15 12:00:00'), now(), now());
+insert into events (title, description, category, suburb, dateTime, createdAt, updatedAt)
+values ('Whiteman Park Treeplanting', 'Whiteman Park Treeplanting description goes here...', 'Tree Planting', 'Whiteman Park, WA', ('2020-08-16 13:00:00'), now(), now());
