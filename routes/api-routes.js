66f829853e3edcb7640a20477767f0e79789daa2
Stitch <john@comteque.com.au>
2020-08-21 19:07:02 +0800

    cleaned up code

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,6 +6,7 @@ const Op = Sequelize.Op;
 const nodemailer = require("nodemailer");
 
 module.exports = function (app) {
+    
     // Using the passport.authenticate middleware with our local strategy.
     // If the user has valid login credentials, send them to the members page.
     // Otherwise the user will be sent an error
